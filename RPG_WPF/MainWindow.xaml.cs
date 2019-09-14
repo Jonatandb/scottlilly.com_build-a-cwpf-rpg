b868516255023677be88f707dfa063e0b9002927
Jonatandb <jonatandb@gmail.com>
2019-09-14 20:02:53 -0300

    Lesson 03.6 – Update Player data with the PropertyChanged event

--- RPG_WPF/MainWindow.xaml.cs
+++ RPG_WPF/MainWindow.xaml.cs
@@ -32,5 +32,10 @@ namespace RPG_WPF
             DataContext = _gameSession;
             
         }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
+        }
     }
 }
