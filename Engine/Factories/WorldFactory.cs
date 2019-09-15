3e9e0221678731e1be68e4c30e97ad92dd95fdc0
Jonatandb <jonatandb@gmail.com>
2019-09-15 12:00:04 -0300

    Lesson 04.2: Creating the World

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+            newWorld.AddLocation(-2, -1, "Campo del granjero",
+                "Aquí hay hileras de maíz, con ratas gigantes que se esconden entre ellas.",
+                "/Engine;component/Images/Locations/FarmFields.png");
+
+            newWorld.AddLocation(-1, -1, "Casa del granjero",
+                "Esta es la casa de tu vecino, el granjero Ted.",
+                "/Engine;component/Images/Locations/Farmhouse.png");
+
+            newWorld.AddLocation(0, -1, "Hogar",
+                "Esta es tu casa",
+                "/Engine;component/Images/Locations/Home.png");
+
+            newWorld.AddLocation(-1, 0, "Tienda de comercio",
+                "La tienda de Susan, la comerciante.",
+                "/Engine;component/Images/Locations/Trader.png");
+
+            newWorld.AddLocation(0, 0, "Plaza de la ciudad",
+                "Ves una fuente aquí.",
+                "/Engine;component/Images/Locations/TownSquare.png");
+
+            newWorld.AddLocation(1, 0, "Puerta de la ciudad",
+                "Aquí hay una puerta que protege al pueblo de las arañas gigantes.",
+                "/Engine;component/Images/Locations/TownGate.png");
+
+            newWorld.AddLocation(2, 0, "Bosque de arañas",
+                "Los árboles en este bosque están cubiertos de telarañas.",
+                "/Engine;component/Images/Locations/SpiderForest.png");
+
+            newWorld.AddLocation(0, 1, "Cabaña del herbolario",
+                "Ves una pequeña cabaña, con plantas secas que caen del techo.",
+                "/Engine;component/Images/Locations/HerbalistsHut.png");
+
+            newWorld.AddLocation(0, 2, "Jardín del herbolario",
+                "Aquí hay muchas plantas, con serpientes escondidas detrás de ellas.",
+                "/Engine;component/Images/Locations/HerbalistsGarden.png");
+
+            return newWorld;
+        }
+    }
+}
---
+++ Engine/Models/World.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        private List<Location> _locations = new List<Location>();
+
+        internal void AddLocation(int xCoord, int yCoord, string name, string description, string imageName)
+        {
+            _locations.Add(
+                new Location() {
+                    XCoordinate = xCoord,
+                    YCoordinate = yCoord,
+                    Name = name,
+                    Description = description,
+                    ImageName = imageName           
+                }
+            );
+        }
+
+        internal Location LocationAt(int xCoord, int yCoord)
+        {
+            foreach( Location loc in _locations )
+            {
+                if( loc.XCoordinate == xCoord && loc.YCoordinate == yCoord )
+                    return loc;
+            }
+            return null;
+        }
+    }
+}
