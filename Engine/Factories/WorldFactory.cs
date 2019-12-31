fde13c0057c7827f88ddf7f8e88e4b1119b78134
Jonatandb <jonatandb@gmail.com>
2019-12-31 15:09:15 -0300

    Lesson 04.5: Improving the World – Factory and Guard Clauses
    Uso de named parameters en GameSession durante la creación del objeto Player.
    
    Se usa el patrón de diseño Factory para hacer static la clase WorldFactory y su método CreateWorld para que no haya que instanciarla desde GameSession.
    
    Refactor a las funciones de movimiento en GameSession para que validen antes de cambiar a la ubicación deseada si efectivamente la nueva ubicación existe.

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -3,9 +3,9 @@ using Engine.Models;
 
 namespace Engine.Factories
 {
-    internal class WorldFactory
+    internal static class WorldFactory
     {
-        internal World CreateWorld()
+        internal static World CreateWorld()
         {
             World newWorld = new World();
             newWorld.AddLocation(-2, -1, "Campo del granjero",
