5dcc910627d6f068603f6cc5ddf7e1198a279e07
Jonatandb <jonatandb@gmail.com>
2020-01-01 20:47:07 -0300

    Lesson 05.1: Creating the Game Item Factory

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,37 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    static class ItemFactory
+    {
+        static List<GameItem> _standardGameItems;
+
+        /// <summary>
+        /// The first time anything is used in ItemFactory, 
+        /// this function will run and populate our 
+        /// _standardGameItems variable with all the items 
+        /// in the game.
+        /// </summary>
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+        }
+
+        static GameItem CreateGameItem(int itemTypeId)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeId == itemTypeId);
+            if(standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+            return null;
+        }
+    }
+}
---
+++ Engine/Models/GameItem.cs
@@ -0,0 +1,28 @@
+﻿// https://scottlilly.com/build-a-cwpf-rpg/lesson-05-1-creating-the-game-item-factory/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class GameItem
+    {
+        public int ItemTypeId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public GameItem(int itemTypeId, string name, int price)
+        {
+            ItemTypeId = itemTypeId;
+            Name = name;
+            Price = price;
+        }
+
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeId, Name, Price);
+        }
+    }
+}
